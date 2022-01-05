f24eb377ca778f1bae72049127c719ba91312cb3
nagatejasharaf832 <nagatejasharaf832@gmail.com>
2022-01-05 13:41:17 -0500

    idk

---
+++ content/jobs/Eunimart/index.md
@@ -0,0 +1,25 @@
+---
+date: '2020-07-30'
+title: 'Software Development Engineer'
+company: 'Eunimart'
+location: 'Hyderbad, IN'
+range: 'Jan 2019 - May 2021'
+url: 'https://eunimart.com/'
+---
+
+- Worked as Python and Nodejs developer with hands on experience in SQL, Linux, Git, and frameworks like
+  Microservices etc.,
+
+- Designed Microservice Architecture and Implemented Orchestration, Choreography Models for our platform.
+
+- Developed database schema design for marketplace microservice in our e-commerce platform.
+
+- Integrated various open APIs from Amazon, Flipkart, Bonanza, eBay etc. into our platform for analyzing new
+  orders, products, inventory, and pricing in all regions of marketplaces.-
+
+- Collected and analyzed product information from various marketplaces to determine competitive pricing of similar
+  products for a seller in our platform.
+
+- Designed complex Json specs in automating data collection from different e-commerce websites.
+
+- Providing open API docs to third party clients using stop light studio tool and Swagger.
--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -59,18 +59,13 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Nagatheja Sharaf.</h2>;
+  const three = <h3 className="big-heading">I love to learn new Technologies.</h3>;
   const four = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        I am a backend software engineer focused on building scalable and efficient software
+        systems.
       </p>
     </>
   );
@@ -79,7 +74,8 @@ const Hero = () => {
       className="email-link"
       href="https://www.newline.co/courses/build-a-spotify-connected-app"
       target="_blank"
-      rel="noreferrer">
+      rel="noreferrer"
+    >
       Check out my course!
     </a>
   );
